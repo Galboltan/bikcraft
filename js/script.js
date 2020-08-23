5bc21d5fb483c4089abe3f2d4811a0d89137855f
Mr.Lexotan <leite.tavares.joao@gmail.com>
2020-08-22 23:33:30 -0300

    Adicionado a parte de sliders da pag de portfólio

---
+++ js/script.js
@@ -0,0 +1,16 @@
+if(window.SimpleSlide) {
+    
+    new SimpleSlide({
+        slide: 'quote',
+        auto: true, 
+        time: 5000, 
+    });
+
+    new SimpleSlide({
+        slide: 'portfolio',
+        auto: true, 
+        time: 5000, 
+        nav: true,
+    });
+
+}
